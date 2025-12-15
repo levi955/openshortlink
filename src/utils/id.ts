a2575fac758a0f666a663c3bac5040649762f91d
idhamsy <msa.csui08@gmail.com>
2025-12-15 20:26:43 +0700

    Initial beta relesase

---
+++ src/utils/id.ts
@@ -0,0 +1,17 @@
+// Utility functions for generating IDs
+
+export function generateId(prefix = ''): string {
+  const timestamp = Date.now().toString(36);
+  const random = Math.random().toString(36).substring(2, 9);
+  return prefix ? `${prefix}_${timestamp}_${random}` : `${timestamp}_${random}`;
+}
+
+export function generateSlug(length = 8): string {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
